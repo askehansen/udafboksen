f0e082133bb2eee9842988e5d0acad53d81c4d36
Aske Hansen <aske@deeco.dk>
2017-10-30 22:28:50 +0100

    fix

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -6,7 +6,7 @@ class MessageMailer < ApplicationMailer
     @eboks_message = message.eboks_message
     @filename = "#{@eboks_message.name.split(' ').first}.#{@eboks_message.format}"
 
-    attachments.inline[@filename] = @eboks_message.content
+    attachments[@filename] = @eboks_message.content
 
     mail to: @user.email, subject: @eboks_message.name, from: "#{@eboks_message.sender} <service@udafboksen.herokuapp.com>"
   end
