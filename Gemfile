f8354209fd897f526fe1157b2f74c6e5cd98f7c3
Aske Hansen <aske@deeco.dk>
2017-10-31 16:54:26 +0100

    upraded webpacker

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'pry-rails'
 gem 'figaro'
 gem 'oga'
 gem 'slim-rails'
-gem 'webpacker'
+gem 'webpacker', '~> 3.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,16 +1,3 @@
-// Note: You must restart bin/webpack-watcher for changes to take effect
+const environment = require('./environment')
 
-var webpack = require('webpack')
-var merge   = require('webpack-merge')
-
-var sharedConfig = require('./shared.js')
-
-module.exports = merge(sharedConfig.config, {
-  output: { filename: '[name]-[hash].js' },
-
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ]
-})
+module.exports = environment.toWebpackConfig()
