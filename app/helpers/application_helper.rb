558d844b0eb937603307217bec918588699f4e68
Aske Hansen <aske@deeco.dk>
2017-11-07 23:54:37 +0100

    new design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  def r_image_tag(name_at_1x, options={})
+    path_at_2x = if options[:path_at_2x]
+      options[:path_at_2x]
+    else
+      name_at_2x = name_at_1x.gsub(%r{\.\w+$}, '@2x\0')
+      asset_path(name_at_2x)
+    end
+
+    image_tag(name_at_1x, options.merge(srcset: "#{path_at_2x} 2x"))
+  end
+
 end
